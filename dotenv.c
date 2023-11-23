ae1a9f01a75f4827761553386bd4bb309e6da4f2
blackmamoth <blackmamoth83@gmail.com>
2023-11-24 00:20:11 +0530

    removed unused variable

--- dotenv.c
+++ dotenv.c
@@ -22,7 +22,6 @@ void load_env(const char *file_path)
     char value[MAX_VALUE_LENGTH];
     char quote = '\0';
     int assigment_index = 0;
-    int line_length = 0;
     int i = 0;
     if ((env = fopen(file_path, "r")) != NULL)
     {
@@ -30,10 +29,8 @@ void load_env(const char *file_path)
         {
             value[0] = '\0';
             assigment_index = 0;
-            line_length = 0;
             i = 0;
             line[strcspn(line, "\n")] = '\0';
-            line_length = strlen(line);
             assigment_index = strcspn(line, EQUAL_SIGN);
             if (line[assigment_index - 1] == ' ')
                 assigment_index--;
